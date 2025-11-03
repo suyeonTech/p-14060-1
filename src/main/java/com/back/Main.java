a789a5f2b094c250ccfe36442cb8da6b6b83827b
suyeonTech <wsy-ofc@naver.com>
2025-11-03 11:31:22 +0900

    level6

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -34,9 +34,17 @@ public class Main {
             }
 
             if (cmd.equals("list")) {
+                System.out.println("----------------------");
                 for (String[] quote : quotes) {
                     System.out.println(quote[0] + " / " + quote[1] + " / " + quote[2]);
                 }
+            }
+
+            if (cmd.startsWith("delete")) {
+                int index2 = cmd.indexOf("=");
+                String what = cmd.substring(index2 + 1);
+                quotes.removeIf(arr -> what.equals(arr[0]));
+                System.out.println("Quote number" + what + " deleted.");
             }
         }
     }
